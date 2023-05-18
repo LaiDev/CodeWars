8249da07fc592956a844fcdeba2a79492bb02186
Aalaizha B <l.bnjamin0309@gmail.com>
2023-05-18 09:26:36 -0400

    ADD : 7kyu

---
+++ 7kyu/strEndsWith.js
@@ -0,0 +1,10 @@
+/* Directions :
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Link : https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
+*/
+
+//My Solution
+
+const solution = (str, ending) => str.endsWith(ending);
